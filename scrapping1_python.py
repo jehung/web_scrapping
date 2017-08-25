f5af1f9540128d4d6ad38b7927f8fdf808b50a7e
jehung <jehung@me.com>
2017-08-24 23:21:22 -0700

    Add ipynb

--- scrapping1_python.py
+++ scrapping1_python.py
@@ -5,7 +5,7 @@
 
 
 import time
-import json
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -22,14 +22,15 @@ def init_driver():
 
 
 def lookup(driver):
-    do_continue = True
-    counter = 0
+    post_dict =  {'link':[], 'title':[], 'stats':[], 'last_post_date':[]}
     driver.get('http://www.f150ecoboost.net/forum/68-2016-ford-f150-ecoboost-chat')
     driver.find_element_by_xpath('''//*[@id="yui-gen11"]''').click()
-
+    counter1 = 0
+    counter2 = 0
+    counter3 = 0
     page_number = 1
 
-    while True:
+    while True==1:
         try:
             link = driver.find_element_by_link_text(str(page_number))
         except NoSuchElementException:
@@ -37,31 +38,47 @@ def lookup(driver):
 
         posts = driver.find_elements_by_class_name('threadtitle')
         for post in posts:
-            print(post.find_element_by_css_selector('a').get_attribute('href'))
-            print(post.text)
+            #print(post.find_element_by_css_selector('a').get_attribute('href'))
+            post_dict['link'].append(post.find_element_by_css_selector('a').get_attribute('href'))
+            #print(post.text)
+            post_dict['title'].append(post.text)
+            counter1 += 1
+        print(counter1)
 
-            #stats = post.find_element_by_xpath('''//*[@class='threadstats td alt']''')
-            #for stat in stats:
-            #print(stats.text)
-
+        #stats_h = driver.find_elements_by_xpath('''//*[@class='threadstats td']''')
+        post_dict['stats'].append(driver.find_elements_by_xpath('''//*[@class='threadstats td']'''))
+        counter2 += 1
         stats = driver.find_elements_by_xpath('''//*[@class='threadstats td alt']''')
         for stat in stats:
-            print(stat.text)
+            #print(stat.text)
+            post_dict['stats'].append(stat.text)
+            counter2 += 1
+        print(counter2)
 
         postdates = driver.find_elements_by_xpath('''//*[@class='threadlastpost td']''')
         for postdate in postdates:
-            print(postdate.text)
+            #print(postdate.text)
+            post_dict['last_post_date'].append(postdate.text.split(','))
+            counter3 += 1
+        print(counter3)
 
-            counter += 1
         link.click()
         print(driver.current_url)
         page_number += 1
-    print(counter)
+
+    return post_dict
 
 
 if __name__ == '__main__':
     driver = init_driver()
-    lookup(driver)
+    data = lookup(driver)
     time.sleep(5)
     driver.quit()
 
+
+    for e in data:
+        print(e)
+        print(len(data[e]))
+    data = pd.DataFrame(data.items())#, columns=['link', 'title', 'stats', 'last_post_date'])
+    #data = pd.DataFrame.from_dict(data)
+    print(data.head())
